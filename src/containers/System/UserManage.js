96eb24bce7a2df0ae63fa6141f3aa1dc4be0b9b7
POHTranQuocThai <tranthai.070104@gmail.com>
2024-12-08 14:04:09 +0700

    Làm giao diện hiện list user eidt & delete

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss'
+import { userService } from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            arrUser: []
+        }
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        const response = await userService.getAllUser('All')
+        console.log('🚀 ~ UserManage ~ componentDidMount ~ response:', response)
+        if (response && response.status === 'OK') {
+            this.setState({
+                arrUser: response.users
+            })
+        }
+        console.log(this.state.arrUser);
 
     }
-
+    /** Life cycle
+     * Run component:
+     * 1.Run Constructer => init state
+     * 2.Did mount (set state)
+     * 3.Render
+     *  
+     */
 
     render() {
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <div className='title'>Manage users with Eric</div>
+                <div className="users-table">
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>Họ</th>
+                            <th>Tên</th>
+                            <th>Địa chỉ</th>
+                            <th>Hành động</th>
+                        </tr>
+                        {this.state.arrUser?.map(user => (
+                            <tr key={user.id}> {/* key cần có giá trị duy nhất */}
+                                <td>{user.email}</td>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                                <td>{user.address}</td>
+                                <td>
+                                    <button className='btn-edit'><i className="fa-solid fa-pen-to-square"></i></button>
+                                    <button className='btn-delete'><i className="fa-solid fa-eraser"></i></button>
+                                </td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,6 +3,10 @@ const handleLogin = async (email, password) => {
     const response = await axios.post('/api/login', { email, password });
     return response.data; // Đảm bảo trả về response.data
 }
+const getAllUser = async (id) => {
+    return (await axios.get(`/api/get-all-users?id=${id}`)).data
+}
 export const userService = {
-    handleLogin
+    handleLogin,
+    getAllUser
 }
